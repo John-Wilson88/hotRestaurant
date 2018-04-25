569e113a64f17cb757c8514c94d9fe2ca8335d21
John <thehumantorch88@gmail.com>
2018-04-24 21:42:55 -0400

    env fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PROT || 3000;
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
